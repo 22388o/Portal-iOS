d9d4764a686cd7691fb5672da372796fdc0fe95e
cuhte3 <farid@getportal.co>
2020-03-09 17:54:28 +0700

    Extensions

---
+++ Portal/Extensions/Double+Extension.swift
@@ -0,0 +1,52 @@
+//
+//  Double+Extension.swift
+//  Portal
+//
+//  Created by Farid on 09.03.2020.
+//  Copyright © 2020 Tides Network. All rights reserved.
+//
+
+import Foundation
+
+extension Double {
+    func btcFormatted() -> String {
+        roundToDecimal(3).toString() + " BTC"
+    }
+    func ethFormatted() -> String {
+        roundToDecimal(3).toString() + " ETH"
+    }
+    func rounded(toPlaces places: Int) -> Double {
+        let divisor = pow(10.0, Double(places))
+        return (self * divisor).rounded() / divisor
+    }
+    func roundToDecimal(_ fractionDigits: Int) -> Double {
+        let multiplier = pow(10, Double(fractionDigits))
+        return Darwin.round(self * multiplier) / multiplier
+    }
+    func toString(decimal: Int = 12) -> String {
+        let value = decimal < 0 ? 0 : decimal
+        var string = String(format: "%.\(value)f", self)
+        
+        while string.last == "0" || string.last == "." {
+            if string.last == "." { string = String(string.dropLast()); break }
+            string = String(string.dropLast())
+        }
+        if string == "0" {
+            string = "0.00"
+        }
+        return string
+    }
+    
+    func fiatFormatted(_ symbol: String? = "$") -> String {
+        let formatter = NumberFormatter()
+        formatter.currencySymbol = symbol
+        formatter.groupingSize = 3
+        formatter.numberStyle = .currency
+        formatter.maximumFractionDigits = 2 //self < 1 ? 2 : 0
+        formatter.minimumFractionDigits = 1
+        formatter.minimumIntegerDigits = 1
+        return formatter.string(from: NSNumber(value: self)) ?? "#"
+    }
+}
+
+
---
+++ Portal/Extensions/PieChart+Extension.swift
@@ -0,0 +1,48 @@
+//
+//  PieChart+Extension.swift
+//  Portal
+//
+//  Created by Farid on 09.03.2020.
+//  Copyright © 2020 Tides Network. All rights reserved.
+//
+
+import Foundation
+import Charts
+import SwiftUI
+
+extension Charts.PieChartView {
+    func applyStandardSettings() {
+        holeRadiusPercent = 0.85
+//        rotationAngle = 25
+        holeColor = nil
+        transparentCircleRadiusPercent = 0
+        legend.enabled = false
+        chartDescription.enabled = false
+        noDataText = "No coins in the wallet"
+        transparentCircleColor = UIColor.clear
+        noDataTextColor = UIColor(white: 0.3, alpha: 0.4)
+        noDataFont = UIFont(name: "Avenir-Medium", size: 12.0)!
+        extraTopOffset = 10
+        extraBottomOffset = 15
+        extraLeftOffset = 10
+        extraRightOffset = 10
+    }
+}
+
+extension Charts.PieChartDataSet {
+    func standardSettings(colors: [UIColor]) {
+        selectionShift = 0
+        sliceSpace = 7
+        xValuePosition = .outsideSlice
+        yValuePosition = .outsideSlice
+        valueTextColor = UIColor(white: 0.3, alpha: 0.6)
+        valueLineColor = UIColor(white: 0.3, alpha: 0.6)
+        entryLabelFont = UIFont(name: "Avenir-Medium", size: 12.0)!
+        valueLineWidth = 2.5
+        valueLinePart1OffsetPercentage = 1.5
+        valueLinePart1Length = 0.4
+        valueLinePart2Length = 0.4
+        drawValuesEnabled = false
+        self.colors = colors
+    }
+}
