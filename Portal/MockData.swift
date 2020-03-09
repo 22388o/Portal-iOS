18ad1a9c2f6533e40e3f8f70791224535c45aa2f
cuhte3 <farid@getportal.co>
2020-03-09 17:54:03 +0700

    Mock data

--- Portal/MockData.swift
+++ Portal/MockData.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import SwiftUI
 
 struct BTC: WalletItemViewModel {
     var name  = "Bitcoin"
@@ -15,6 +16,17 @@ struct BTC: WalletItemViewModel {
     var totalValue = "18353.23"
     var price = "$9125"
     var change = "+0.25%"
+    var color = UIColor.green
+}
+
+struct BCH: WalletItemViewModel {
+    var name  = "Bitcoin Cash"
+    var symbol = "BCH"
+    var amount = "10.2502"
+    var totalValue = "3220.23"
+    var price = "$322.23"
+    var change = "+2.15%"
+    var color = UIColor.gray
 }
 
 struct ETH: WalletItemViewModel {
@@ -24,8 +36,33 @@ struct ETH: WalletItemViewModel {
     var totalValue = "4353.23"
     var price = "$215"
     var change = "+6.25%"
+    var color = UIColor.blue
+}
+
+struct LPT: WalletItemViewModel {
+    var name  = "Livepeer Token"
+    var symbol = "LPT"
+    var amount = "2.13"
+    var totalValue = "3.16"
+    var price = "$215"
+    var change = "+0.02%"
+    var color = UIColor.yellow
 }
 
 struct CoinMock: WalletItemViewModel {}
 
-let WalletMock: [WalletItemViewModel] = [BTC(), ETH(), CoinMock(), BTC(), ETH()]
+let WalletMock: [WalletItemViewModel] = [BTC(), BCH(), ETH(), LPT(), CoinMock()]
+
+enum Currency: Int {
+    case usd
+    case btc
+    case eth
+    
+    func stringValue() -> String {
+        switch self {
+        case .usd: return "USD"
+        case .btc: return "BTC"
+        case .eth: return "ETH"
+        }
+    }
+}
