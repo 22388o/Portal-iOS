c9c8e1dd9a22d5899590f79a179d0832fd674121
cuhte3 <farid@getportal.co>
2020-03-11 14:17:44 +0700

    modal views mocks

---
+++ Portal/Views/Wallet/CoinView/ModalViews/SendToExchangeView.swift
@@ -0,0 +1,23 @@
+//
+//  SendToExchangeView.swift
+//  Portal
+//
+//  Created by Farid on 11.03.2020.
+//  Copyright © 2020 Tides Network. All rights reserved.
+//
+
+import SwiftUI
+
+struct SendToExchangeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#if DEBUG
+struct SendToExchangeView_Previews: PreviewProvider {
+    static var previews: some View {
+        SendToExchangeView()
+    }
+}
+#endif
