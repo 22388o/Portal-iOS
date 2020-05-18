0295a0275c7a265601c3180ec4c5efd66ad5e818
cuhte3 <farid@getportal.co>
2020-05-18 15:45:44 +0700

    CreateWalletViewModel

---
+++ Portal/ViewModels/CreateWalletViewModel.swift
@@ -0,0 +1,38 @@
+//
+//  CreateWalletViewModel.swift
+//  Portal
+//
+//  Created by Farid on 14.05.2020.
+//  Copyright © 2020 Tides Network. All rights reserved.
+//
+
+import Foundation
+import Combine
+
+final class CreateWalletViewModel: ObservableObject {
+    @Published var name: String = ""
+    @Published var walletDataValidated = false
+    @Published var selectorIndex = BtcAddressFormat.segwit.rawValue
+    
+    private var subscription: AnyCancellable?
+        
+    func setup() {
+        subscription = $name
+        .debounce(for: .seconds(0.25), scheduler: DispatchQueue.main)
+        .sink { [weak self] name in
+            self?.walletDataValidated = name.count > 3
+        }
+    }
+    
+    func newModel() -> NewWalletModel {
+        .init(
+            name: name,
+            addressType: BtcAddressFormat.init(rawValue: selectorIndex) ?? .segwit
+        )
+    }
+    
+    deinit {
+        subscription?.cancel()
+        subscription = nil
+    }
+}
