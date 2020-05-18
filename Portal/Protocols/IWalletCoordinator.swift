941d238778e934babeedc29033a097e7709e7035
cuhte3 <farid@getportal.co>
2020-05-18 16:19:42 +0700

    IWalletCoordinator

---
+++ Portal/Protocols/IWalletCoordinator.swift
@@ -0,0 +1,18 @@
+//
+//  IWalletCoordinator.swift
+//  Portal
+//
+//  Created by Farid on 14.05.2020.
+//  Copyright © 2020 Tides Network. All rights reserved.
+//
+
+import Foundation
+
+protocol IWalletCoordinator {
+    var currentWallet: IWallet? { get }
+    var wallets: [IWallet]? { get }
+    
+    func createWallet(model: NewWalletModel)
+    func restoreWallet()
+    func setupWallet(id: String)
+}
