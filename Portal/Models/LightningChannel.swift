f6fbc9dc9680605e21091ebe0c4c77e97fe3123c
cuhte3 <farid@getportal.co>
2022-05-14 22:07:43 +0400

    Create LightningChannel.swift

---
+++ Portal/Models/LightningChannel.swift
@@ -0,0 +1,44 @@
+//
+//  LightningChannel.swift
+//  Portal
+//
+//  Created by farid on 5/12/22.
+//  Copyright © 2022 Tides Network. All rights reserved.
+//
+
+import Foundation
+
+class LightningChannel: Identifiable {
+    enum State: Int16 {
+        case waitingFunds = 0, open, closed
+        
+        var description: String {
+            switch self {
+            case .waitingFunds:
+                return "Funding"
+            case .open:
+                return "Open"
+            case .closed:
+                return "Closed"
+            }
+        }
+    }
+    let id: UInt16
+    let nodeAlias: String
+    let satValue: UInt64
+    var state: State
+    
+    init(id: Int16, satValue: Int64, state: State, nodeAlias: String) {
+        self.id = UInt16(id)
+        self.nodeAlias = nodeAlias
+        self.satValue = UInt64(satValue)
+        self.state = state
+    }
+    
+    init(record: DBLightningChannel) {
+        self.id = UInt16(record.channelID)
+        self.nodeAlias = record.node.alias
+        self.satValue = UInt64(record.satValue)
+        self.state = State.init(rawValue: record.state)!
+    }
+}
