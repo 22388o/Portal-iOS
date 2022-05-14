48b7dc72a4e2e48d322cd87a20401d187ddf705d
cuhte3 <farid@getportal.co>
2022-05-14 22:03:30 +0400

    missing protocols

---
+++ Portal/Protocols/BitcoinCore + Holder.swift
@@ -0,0 +1,13 @@
+//
+//  BitcoinCore + Holder.swift
+//  Portal
+//
+//  Created by Farid on 15.07.2021.
+//
+
+import BitcoinCore
+import Hodler
+
+protocol IBitcoinPluginData: IPluginData {}
+
+extension HodlerData: IBitcoinPluginData {}
