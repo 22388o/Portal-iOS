19d33ff7ab59a943b3d5d1181ef1af8953643efb
cuhte3 <farid@getportal.co>
2022-05-16 02:52:55 +0400

    Update TestPersister.swift

--- Portal/Services/ldk-related/TestPersister.swift
+++ Portal/Services/ldk-related/TestPersister.swift
@@ -10,13 +10,7 @@ import Foundation
 class TestPersister: Persist {
     
     func readChannelMonitors() {
-//        if var channelsDict = UserDefaults.standard.object(forKey: "channelMonitors") as? [[String:Data]] {
-//            for monitorData in channelsDict {
-//                let idBytes: [UInt8] = channelsDict["monitorID"].bytes
-//                let monitorData: [UInt8] = channelsDict["monitorData"].bytes
-////                let mon = ChannelMonitor(pointer: LDKChannelMonitor(channel_id: idBytes, data: monitorData, update_id: 12))
-//            }
-//        }
+
     }
     
     override func persist_new_channel(channel_id: OutPoint, data: ChannelMonitor, update_id: MonitorUpdateId) -> Result_NoneChannelMonitorUpdateErrZ {
@@ -24,16 +18,7 @@ class TestPersister: Persist {
         let monitorBytes: [UInt8] = data.write()
         
         let defaults = UserDefaults.standard
-        
-        if var channelsDict = defaults.object(forKey: "channelMonitors") as? [[String:Data]] {
-            channelsDict.append(
-                ["monitorID" : Data(idBytes),
-                 "monitorData" : Data(monitorBytes)]
-            )
-        } else {
-            let monitorsData = [["monitorID" : Data(idBytes), "monitorData" : Data(monitorBytes)]]
-            defaults.set(monitorsData, forKey: "channelMonitors")
-        }
+        defaults.setValue(monitorBytes, forKey: idBytes.description)
         
         return Result_NoneChannelMonitorUpdateErrZ.ok()
     }
@@ -41,6 +26,10 @@ class TestPersister: Persist {
     override func update_persisted_channel(channel_id: OutPoint, update: ChannelMonitorUpdate, data: ChannelMonitor, update_id: MonitorUpdateId) -> Result_NoneChannelMonitorUpdateErrZ {
         let idBytes: [UInt8] = channel_id.write()
         let monitorBytes: [UInt8] = data.write()
+        
+        let defaults = UserDefaults.standard
+        defaults.setValue(monitorBytes, forKey: idBytes.description)
+        
         return Result_NoneChannelMonitorUpdateErrZ.ok()
     }
     
