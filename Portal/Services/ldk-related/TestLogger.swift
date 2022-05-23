b481c81b4e958f6a9f9881f88b3ce5fec96b3366
cuhte3 <farid@getportal.co>
2022-05-23 11:51:57 +0400

    Logger data separation

--- Portal/Services/ldk-related/TestLogger.swift
+++ Portal/Services/ldk-related/TestLogger.swift
@@ -8,7 +8,23 @@
 class TestLogger: Logger {
 
     override func log(record: Record) {
-//        print("\nRLLogger (\(record.get_level())): \(record.get_file()):\(record.get_line()):\n> \(record.get_args())\n")
+        let messageLevel = record.get_level()
+        let arguments = record.get_args()
+        
+        switch messageLevel {
+        case LDKLevel_Debug:
+            print("\nDebug Logger:\n>\(arguments)\n")
+        case LDKLevel_Info:
+            print("\nInfo Logger:\n>\(arguments)\n")
+        case LDKLevel_Warn:
+            print("\nWarn Logger:\n>\(arguments)\n")
+        case LDKLevel_Error:
+            print("\nError Logger:\n>\(arguments)\n")
+        case LDKLevel_Sentinel:
+            print("\nSentinel Logger:\n>\(arguments)\n")
+        default:
+            break
+        }
     }
 
 }
